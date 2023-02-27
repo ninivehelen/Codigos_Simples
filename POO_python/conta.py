6765453ae3a822d678945e26ac5d01391a0fff46
Nínive Helen <ninivehelen@gmail.com>
2023-02-27 11:03:37 -0300

    arquivo poo conta.py criação de encapsulamento

--- POO_python/conta.py
+++ POO_python/conta.py
@@ -1,32 +1,34 @@
 class Conta():
   def __init__(self,numero, titular, saldo, limite):
-     print("conta")
-     self.numero = numero
-     self.titulo = titular
-     self.saldo = saldo
-     self.limite = limite
+     #Duas ___ é para privar o atributo, python não possui uma palavra chave, possui __ para privar o atributo
+     self.__numero = numero
+     self.__titular = titular
+     self.__saldo = saldo
+     self.__limite = limite
 
   def extrato(self):
-    print(f'O saldo da sua conta é:',self.saldo)
-
+    print(f'O saldo da sua conta é:',self.__saldo,self.__titular)
+  
   def depositar(self,valor):
-     self.saldo += valor
+     self.__saldo += valor
 
   def sacar(self,valor):
-    self.saldo -= valor
-
+    self.__saldo -= valor
+  
+  #Encapsulamento usando um metodo para fazer ações dentro do metodo.
+  def transfere(self,valor_transfere, destino):
+     self.sacar(valor_transfere)
+     destino.depositar(valor_transfere)
+  
+  def eh_inadiplente(self, cliente):
+      pass
+     
 conta = Conta(1,'Paula',1000,2000)
 conta.extrato()
-conta.depositar(20)
+
+conta2 = Conta(2,'Marcos', 100.0, 1000.0)
+conta2.extrato()
+
+conta2.transfere(10, conta)
 conta.extrato()
-conta.sacar(4)
-conta.extrato()
-
-
-
-
-
-
-
-# conta = Conta(1, "Fulano", 0.0, 1000.0)
-# print(conta)
+conta2.extrato()
